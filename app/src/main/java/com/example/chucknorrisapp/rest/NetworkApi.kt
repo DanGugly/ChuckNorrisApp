844146b61c84af40b84e556d8d57797b3a3c67e7
DanGugly <elijahmckoy1997@hotmail.com>
2021-12-17 08:30:38 -0500

    Added fragment for main page

--- app/src/main/java/com/example/chucknorrisapp/rest/NetworkApi.kt
+++ app/src/main/java/com/example/chucknorrisapp/rest/NetworkApi.kt
@@ -3,22 +3,23 @@ package com.example.chucknorrisapp.rest
 import com.example.chucknorrisapp.model.Jokes
 import com.example.chucknorrisapp.model.Value
 import io.reactivex.Single
+import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NetworkApi {
 
     @GET(JOKES+JOKE_COUNT)
-    fun getRandomJokes() : Single<Jokes>
+    suspend fun getRandomJokes() : Response<Jokes>
 
     @GET(JOKES)
-    fun getRandomJoke() : Single<Value>
+    suspend fun getRandomJoke() : Response<Value>
 
     @GET(JOKES)
-    fun getNewCharJokes(
+    suspend fun getNewCharJokes(
         @Query("firstName") first : String,
         @Query("lastName") last : String
-    ) : Single<Value>
+    ) : Response<Value>
 
     companion object{
         const val BASE_URL = "http://api.icndb.com/"
